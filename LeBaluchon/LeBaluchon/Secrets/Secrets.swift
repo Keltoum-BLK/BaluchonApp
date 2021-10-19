6f9c021248797881d5857f9855f5d56cd7afd6f1
Keltoum-BLK <keltoum.belkadi@gmail.com>
2021-10-19 12:33:03 +0200

    try to fix .gitignore

--- LeBaluchon/LeBaluchon/Secrets/Secrets.swift
+++ LeBaluchon/LeBaluchon/Secrets/Secrets.swift
@@ -9,8 +9,8 @@ import Foundation
 
 struct SecretsKeys {
     
-    static let apiKeyCurrency = "03479052e9afb24dc5416b800447e4ad"
-    static let apiKeyWeather = "d39aa9247aa0e8e120ee04f68df6ff6b"
-    static let apiKeyTranslate = "AIzaSyCo95wNsNcsUAMC0PIowXUQjEtnSFx5sjk"
+    static let apiKeyCurrency = ""
+    static let apiKeyWeather = ""
+    static let apiKeyTranslate = ""
 
 }
